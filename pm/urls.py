44f4ae52edf94b86049c81f1a2b4e6df9dab2038
Davide Cavallari <davide.cavallari@gmail.com>
2017-06-23 14:10:34 +0000

    Added save view.

--- pm/urls.py
+++ pm/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+# ex: /pm/
     url(r'^$', views.index, name='index'),
+# ex: /pm/save/
+    url(r'^save/$', views.save, name='save'),
 ]
--- pm/views.py
+++ pm/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 # from django.shortcuts import render
 
+from .models import Measurement
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, you're at the pm index.")
+
+def save(request):
+    return HttpResponse("Hello, you're at the save view.")
