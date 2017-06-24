b3618d8b6de508200c202078cc4a149f56cede03
Davide Cavallari <davide.cavallari@gmail.com>
2017-06-24 05:48:47 +0000

    Written prototyper for save view.

--- pm/models.py
+++ pm/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 class Measurement(models.Model):
     time = models.DateTimeField('date & time')
-    lat = models.DecimalField('latitude', max_digits=8, decimal_places=6)
-    lon = models.DecimalField('longitude', max_digits=8, decimal_places=6)
-    ele = models.DecimalField('elevation', max_digits=5, decimal_places=1)
+    lat = models.DecimalField('latitude', max_digits = 8, decimal_places = 6, null = True)
+    lon = models.DecimalField('longitude', max_digits = 8, decimal_places = 6, null = True)
+    ele = models.DecimalField('elevation', max_digits = 5, decimal_places = 1, null = True)
     pm25 = models.DecimalField('PM2.5', max_digits=4, decimal_places=1)
     pm10 = models.DecimalField('PM10', max_digits=4, decimal_places=1)
+    def __str__(self):
+        s = "time = " + str(self.time) + ", lat = " + str(self.lat) + ", lon = " + str(self.lon) + ", ele = " + str(self.ele) + ", pm25 = " + str(self.pm25) + ", pm10 = " + str(self.pm10)
+        return s
 
 class User(models.Model):
     email = models.EmailField()
--- pm/views.py
+++ pm/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.utils import dateparse
 # from django.shortcuts import render
 
 from .models import Measurement
@@ -8,4 +9,26 @@ def index(request):
     return HttpResponse("Hello, you're at the pm index.")
 
 def save(request):
-    return HttpResponse("Hello, you're at the save view.")
+    now = request.GET['now']
+#    lat = request.GET['lat']
+#    lon = request.GET['lon']
+#    ele = request.GET['ele']
+    pm25 = request.GET['pm25']
+    pm10 = request.GET['pm10']
+
+
+    time = dateparse.parse_datetime(now)
+
+    measurement = Measurement(time = time,
+#                              lat = lat,
+#                              lon = lon,
+#                              ele = ele,
+                              pm25 = pm25,
+                              pm10 = pm10)
+    measurement.save()
+
+    responseText = "Hello, you're at the save view.\n"
+    responseText += str(Measurement.objects.all())
+
+
+    return HttpResponse(responseText)
